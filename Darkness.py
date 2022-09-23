f0ed3387171b37397d3f98694303c766a5d28718
rayquan <rayquan12@gmail.com>
2022-09-23 02:15:05 -0400

    dark library

---
+++ Darkness.py
@@ -0,0 +1,43 @@
+from colorama import Fore, Style
+import os
+import shutil
+
+def main():
+    pass
+
+def create_directory(p_name):
+    # checks to see if a directory exists. If it doesn't, it makes a new one.
+    screenshots_dir = os.getcwd() + '\LakeDarkness\crypto_projects\{}'.format(p_name)
+    
+    isExist = os.path.exists(screenshots_dir)
+    
+    if not isExist:
+        os.makedirs(screenshots_dir)
+        os.makedirs(screenshots_dir + "\community")
+        os.makedirs(screenshots_dir + "\development")
+        os.makedirs(screenshots_dir + "\\tokenomics")
+
+        print(Fore.GREEN + "Successfully created:", screenshots_dir)
+        print("Successfully created:", screenshots_dir + "\community")
+        print("Successfully created:", screenshots_dir + "\development")
+        print("Successfully created:", screenshots_dir + "\\tokenomics" + Style.RESET_ALL)
+    
+    else:
+        print("A directory for {} exists. Replace?".format(p_name))
+
+        y_list = ("y", "yes", "Y", "YES", "Yes")
+        
+        replace = input()
+
+        if replace in y_list:
+            shutil.rmtree(screenshots_dir)
+
+            print(Fore.YELLOW + "Previous project data removed: {}.".format(p_name))
+            print(">> Creating new directory...\n" + Style.RESET_ALL)
+            
+            create_directory(p_name)     
+        else:
+            print(Fore.RED + "Declined action." + Style.RESET_ALL)
+
+if __name__=='__main__':
+    main()
