d12cf3fffad8842885ad5642c933de815fa0cd0e
rayquan <rayquan12@gmail.com>
2022-09-24 16:21:30 -0400

    Notes, search library, screenshot
    added tools (need func)

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import Darkness as dark
+import googlesearch
+
+from time import sleep
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+
+
 
 def main():
-    print("Input a project name:")
-    project_name = str(input().lower())
+    
+    # Insert a project name to create directory
+    #print("Input a project name:")
+    #project_name = str(input().lower())
 
-    dark.create_directory(project_name)
+    #dark.create_directory(project_name)
 
     # OSINT search on crypto project
+    #new_search = googlesearch.search("bitcoin", num_results=10, lang="en") 
+
+    #num = 0
+    #for n in new_search:
+        #num = num + 1
+        #print("{}: {}".format((num), n))
+
     # Screenshot results
+    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+    driver.get("http://www.python.org")
+
+    sleep(1)
+    driver.get_screenshot_as_file("screenshot.png")
+
+    driver.close()
     # Put screenshots in the directory
 
 
