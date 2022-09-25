569b5c9e8287641d6422975f12d56a52e1c6dfad
rayquan <rayquan12@gmail.com>
2022-09-25 11:10:59 -0400

    updated reqs

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import Darkness as dark
-import googlesearch
 
 from time import sleep
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
-
-
 def main():
     
-    # Insert a project name to create directory
-    #print("Input a project name:")
-    #project_name = str(input().lower())
+    # Insert a project name
+    print("Input a project name:")
+    project_name = str(input().lower())
 
+    # Create project directory.
     #dark.create_directory(project_name)
 
-    # OSINT search on crypto project
-    #new_search = googlesearch.search("bitcoin", num_results=10, lang="en") 
+    project_urls = dark.project_search(project_name)
+    
+    num = 0
+    for n in project_urls:
+        num = num + 1
+        print("{}: {}".format((num), n))
 
-    #num = 0
-    #for n in new_search:
-        #num = num + 1
-        #print("{}: {}".format((num), n))
+    
 
     # Screenshot results
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
