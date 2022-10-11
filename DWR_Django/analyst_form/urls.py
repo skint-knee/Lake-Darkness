00a926d85c12052527ec2b58c54e9f4fbdd3371e
RayQuan <rayquan12@gmail.com>
2022-10-11 16:42:22 -0400

    updated root url to project-form

--- DWR_Django/analyst_form/urls.py
+++ DWR_Django/analyst_form/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     # pk is a dynamic string value
+    path('', views.project_form, name="project-form"),
     path('projects/<str:pk>/', views.projects, name="projects"),
-    path('project-form/', views.project_form, name="project-form"),
+    
 ]
