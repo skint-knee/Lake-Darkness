a0f61eb44f502a7e9ffa10810795d79f88f5aa93
rayquan <rayquan12@gmail.com>
2022-10-22 23:28:16 -0400

    quering data from coingecko and displaying it in the template

--- DWR_Django/analyst_form/views.py
+++ DWR_Django/analyst_form/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pycoingecko import CoinGeckoAPI
+# instantiating the coingecko api
+cg = CoinGeckoAPI()
 # we defined the projects function to require a parameter "pk" or primary key
 # these functions are technically views and shouldn't be here
 def projects(request, pk):
     return HttpResponse('Here is the basic response {}'.format(pk))
  
 def project_form(request):
-    return render(request, 'analyst_form/analyst_form.html')
+
+    msg = cg.get_price(ids='bitcoin', vs_currencies='usd')
+    bitcoin_price = msg['bitcoin']['usd']
+    f_bitcoin_price = f"The price of Bitcoin is ${bitcoin_price:,.2f}."
+
+    context = {'message':f_bitcoin_price}
+    return render(request, 'analyst_form/analyst_form.html', context)
